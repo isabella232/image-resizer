d90a1fca536413ffb1ad232788fabcfb6b5fac39
James Wheare <james@wheare.org>
2015-03-18 20:06:31 +0000

    Fix another deprecation warning

--- src/streams/response.js
+++ src/streams/response.js
@@ -107,7 +107,7 @@ ResponseWriter.prototype._write = function(image){
       image.log.log('checksum', shasum.digest('hex'));
     }
 
-    this.response.send(200, image.contents);
+    this.response.status(200).send(image.contents);
   }
 
   // flush the log messages and close the connection
